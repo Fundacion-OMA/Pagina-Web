6b89af00540b408929b68ce69afc9b88ab8d83bf
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2025-02-11 18:14:50 -0300

    fix: background and nav gap

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <nav className="bg-oma-ligthgray box flex px-[25px] py-[20px]">
       <img className="mr-auto" src="/icons/hamb_menu.svg"/>
-      <div className="flex gap-[36px] flex-row">
+      <div className="flex gap-[38px] flex-row">
         <img src="/icons/bell.svg"/>
         <img src="/icons/faq.svg"/>
         <img src="/icons/user.svg"/>
