7104a5133d784303523764d337df3cfda648f079
JZylber <zylberjulian@gmail.com>
2025-02-11 18:56:44 -0300

    feat: font and nav adapted to roles

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,15 +1,29 @@
+import useAuth from "../hooks/useAuth";
+import { Roles } from "../stores/user";
 import RoleSelect from "./RoleSelect";
 
 const Navbar = () => {
+  const { user } = useAuth();
+  const { role } = user;
+  const isAnon = role === Roles.ANON;
   return (
     <div className="flex flex-col gap-y-2">
       <RoleSelect />
-      <nav className="bg-oma-ligthgray box flex px-[25px] py-[20px]">
+      <nav className="bg-oma-ligthgray box flex px-[25px] py-[20px] box-content h-[32px]">
         <img className="mr-auto" src="/icons/hamb_menu.svg" />
-        <div className="flex gap-[38px] flex-row">
-          <img src="/icons/bell.svg" />
+        <div
+          className={`flex ${
+            isAnon ? "gap-[20px]" : "gap-[38px]"
+          } flex-row items-center`}
+        >
+          {!isAnon && <img src="/icons/bell.svg" />}
           <img src="/icons/faq.svg" />
-          <img src="/icons/user.svg" />
+          {!isAnon && <img src="/icons/user.svg" />}
+          {isAnon && (
+            <a className="text-black text-[25px] font-bold font-[Neue_Regrade]">
+              Ingresar
+            </a>
+          )}
         </div>
       </nav>
     </div>
