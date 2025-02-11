6cfba7f98c49f0449b58e0cd6bbf1644c23dedf5
JZylber <zylberjulian@gmail.com>
2025-02-11 18:36:00 -0300

    fix(nav): temporary move of RoleSelect

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,15 +2,17 @@ import RoleSelect from "./RoleSelect";
 
 const Navbar = () => {
   return (
-    <nav className="bg-oma-ligthgray box flex px-[25px] py-[20px]">
-      <img className="mr-auto" src="/icons/hamb_menu.svg"/>
-      <div className="flex gap-[38px] flex-row">
-        <img src="/icons/bell.svg"/>
-        <img src="/icons/faq.svg"/>
-        <img src="/icons/user.svg"/>
-        <RoleSelect />
-      </div>
-    </nav>
+    <div className="flex flex-col gap-y-2">
+      <RoleSelect />
+      <nav className="bg-oma-ligthgray box flex px-[25px] py-[20px]">
+        <img className="mr-auto" src="/icons/hamb_menu.svg" />
+        <div className="flex gap-[38px] flex-row">
+          <img src="/icons/bell.svg" />
+          <img src="/icons/faq.svg" />
+          <img src="/icons/user.svg" />
+        </div>
+      </nav>
+    </div>
   );
 };
 
