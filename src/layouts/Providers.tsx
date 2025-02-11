7be83c22556c4da8a5a3b9731412c286191a0c14
JZylber <zylberjulian@gmail.com>
2025-02-11 17:17:14 -0300

    refactor(auth): context switched to nanostores

--- src/layouts/Providers.tsx
+++
@@ -1,8 +0,0 @@
-import type { ReactNode } from "react";
-import { AuthProvider } from "../components/contexts/UserContext";
-
-const Providers: React.FC<{ children: ReactNode }> = ({ children }) => {
-  return <AuthProvider>{children}</AuthProvider>;
-};
-
-export default Providers;
