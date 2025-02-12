fcd9ea7942316054454eebddd384eef8533b5639
JZylber <zylberjulian@gmail.com>
2025-02-11 23:58:52 -0300

    feat(home): folder component

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,6 +1,7 @@
 import useAuth from "../hooks/useAuth";
 import { Roles } from "../stores/user";
 import BoxButton from "../utils/box/BoxButton";
+import Folder from "../utils/folder/Folder";
 
 const Home = () => {
   const { user } = useAuth();
@@ -19,6 +20,25 @@ const Home = () => {
       <main className="grid grid-cols-2 gap-y-[20px] gap-x-[25px]">
         {isAnon && (
           <>
+            <Folder
+              title="¿Qué es la OMA?"
+              className="col-span-full"
+              titleClassName="bg-oma-purple"
+              bodyClassName="bg-oma-lightpurple"
+              seeMoreLink="/novedades"
+            >
+              <p className="text-black text-xl font-normal font-['Neue Regrade']">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+                cursus dignissim rutrum. Curabitur sollicitudin hendrerit lorem,
+                ut tempus urna luctus et. Etiam tincidunt lobortis iaculis.
+                Praesent interdum erat nec aliquam lobortis. Donec viverra lorem
+                arcu, non viverra odio sodales id. Sed tempor purus at ante
+                maximus lobortis. Nulla facilisis lacus id odio laoreet, ac
+                euismod nisl interdum. Ut tellus ipsum, consectetur ut tellus
+                non, auctor consectetur eros. Proin blandit convallis mollis.
+                Sed eu justo erat.
+              </p>
+            </Folder>
             <BoxButton
               url="/aportes"
               iconLink="/icons/contribute.svg"
@@ -39,7 +59,7 @@ const Home = () => {
           <BoxButton
             url="/dashboard"
             iconLink="/icons/register.svg"
-            text="Registrá a tus alumnos"
+            text="Inscribí a tus alumnos"
             className="bg-oma-shilo col-span-full text-3xl"
           />
         )}
@@ -63,6 +83,27 @@ const Home = () => {
           text="Problemas Viejos"
           className="bg-oma-algaegreen text-2xl"
         />
+        {!isAnon && (
+          <Folder
+            title="Novedades"
+            className="col-span-full"
+            titleClassName="bg-oma-yellow"
+            bodyClassName="bg-oma-lightyellow"
+            seeMoreLink="/novedades"
+          >
+            <p className="text-black text-xl font-normal font-['Neue Regrade']">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+              cursus dignissim rutrum. Curabitur sollicitudin hendrerit lorem,
+              ut tempus urna luctus et. Etiam tincidunt lobortis iaculis.
+              Praesent interdum erat nec aliquam lobortis. Donec viverra lorem
+              arcu, non viverra odio sodales id. Sed tempor purus at ante
+              maximus lobortis. Nulla facilisis lacus id odio laoreet, ac
+              euismod nisl interdum. Ut tellus ipsum, consectetur ut tellus non,
+              auctor consectetur eros. Proin blandit convallis mollis. Sed eu
+              justo erat.
+            </p>
+          </Folder>
+        )}
       </main>
     </>
   );
