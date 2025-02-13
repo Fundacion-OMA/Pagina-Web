0c4c0ca1cfd999e2b21ae5544cdc1a6c31a33c56
JZylber <zylberjulian@gmail.com>
2025-02-13 18:14:55 -0300

    feat(home): folder tab

--- src/components/utils/folder/Folder.tsx
+++ src/components/utils/folder/Folder.tsx
@@ -16,24 +16,32 @@ const Folder: React.FC<FolderProps> = ({
   bodyClassName,
 }) => {
   return (
-    <div className={`${className} overflow-hidden  box w-full flex flex-col`}>
-      <div className={`py-[20px] px-[15px] ${titleClassName}`}>
-        <h3 className="text-black text-[32px] font-bold font-['Neue_Regrade']">
-          {title}
-        </h3>
-      </div>
-      <div className={`${bodyClassName} border-t-2 border-black p-[20px] `}>
-        <div className="overflow-hidden text-ellipsis h-[125px]">
-          {children}
+    <div className={`${className} w-full relative mt-[33px]`}>
+      <div className={` box flex flex-col overflow-hidden z-0`}>
+        <div className={`py-[20px] px-[15px] ${titleClassName} `}>
+          <h3 className="text-black text-[32px] font-bold font-['Neue_Regrade']">
+            {title}
+          </h3>
+          <div className="absolute bottom-[calc(100%-4px)] left-[42px] z-20 folder-tab bg-inherit h-[33px] w-[157px]"></div>
+          <img
+            className="absolute bottom-[calc(100%-3px)] left-[42px] z-20"
+            src="/images/folder_tab.svg"
+          />
+          <div className="absolute bottom-[calc(100%-4px)] left-[42px] z-30 bg-inherit h-[2px] w-[157px]"></div>
         </div>
-        {seeMoreLink && (
-          <a
-            href={seeMoreLink}
-            className="text-black text-xl font-medium font-['Neue_Regrade'] underline"
-          >
-            Ver más
-          </a>
-        )}
+        <div className={`${bodyClassName} border-t-2 border-black p-[20px] `}>
+          <div className="overflow-hidden text-ellipsis h-[125px]">
+            {children}
+          </div>
+          {seeMoreLink && (
+            <a
+              href={seeMoreLink}
+              className="text-black text-xl font-medium font-['Neue_Regrade'] underline"
+            >
+              Ver más
+            </a>
+          )}
+        </div>
       </div>
     </div>
   );
